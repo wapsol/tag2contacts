6b72af442f610120a0a425e8af497e92f2626806
Simplify-ERP® <github@euroblaze.de>
2023-05-12 19:30:20 +0200

    Update res_partner.py

--- models/res_partner.py
+++ models/res_partner.py
@@ -1,1 +1,8 @@
+from odoo import models, fields, api
 
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    @api.model
+    def get_partners_by_tag(self, tag_id):
+        return self.search([('category_id', 'in', tag_id)])
